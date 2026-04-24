a496a1c2e04b25ccd3bd3e1af76366eb97eeb932
takekaraka <takebcn@gmail.com>
2026-04-24 09:53:05 +0200

    🔧 Fix CORS

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // ─── Middleware ───
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
